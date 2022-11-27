46c35cb8c3c8e462f192e39bad3c55965fd674e7
PedroDeVvV <pedrohgs2004@gmail.com>
2022-11-27 13:03:55 -0300

    timer

---
+++ .github/README.md
@@ -0,0 +1,19 @@
+# Timer em JavaScript
+
+#Objetivo✍️
+
+Desenvolvi um projeto de timer com o intuito de praticar o DOM em JS, funções como setInterval e setTimeout e também o objeto Date. 
+
+![preview](./timer.gif)
+
+## 🛠️Tecnologias
+
+* JavaScript
+* HTML
+* CSS
+* Visual Studio Code
+
+## 📞 Contato
+
+pedrohgs2004@gmail.com <br>
+Linkedin: https://www.linkedin.com/in/pedro-henrique-g-silva-a2100a23a/?originalSubdomain=br
---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+function relogio() {
+
+    function pegarHoraSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-br', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+
+    const titulo = document.querySelector('h1');
+    const relogio = document.querySelector('.relogio');
+    const iniciar = document.querySelector('.iniciar');
+    const pausar = document.querySelector('.pausar');
+    const zerar = document.querySelector('.zerar');
+    let segundos = 0;
+    let timer;
+
+    function iniciaRelogio() {
+        timer = setInterval(function () {
+            segundos++;
+            relogio.innerHTML = pegarHoraSegundos(segundos);
+        }, 1000);
+    }
+
+    iniciar.addEventListener('click', function (e) {
+        clearInterval(timer);
+        iniciaRelogio();
+        relogio.classList.remove('timerPausado');
+        titulo.innerHTML = "Timer em Execução...";
+    });
+
+    pausar.addEventListener('click', function (e) {
+        setTimeout(clearInterval(timer));
+        relogio.classList.add('timerPausado');
+        titulo.innerHTML = "Timer Pausado";
+    });
+
+    zerar.addEventListener('click', function (e) {
+        segundos = 0;
+        relogio.innerHTML = "00:00:00";
+        titulo.innerHTML = "Timer";
+        relogio.classList.remove('timerPausado')
+    });
+
+}
+
+relogio();
