bfe25b987ff09af31e852bc3e6184de8b0eb7479
PedroDeVvV <pedrohgs2004@gmail.com>
2022-12-15 13:01:54 -0300

    Timer piscante pausado

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,8 @@ function relogio() {
     const zerar = document.querySelector('.zerar');
     let segundos = 0;
     let timer;
+    let colocaRed;
+    let removeRed;
 
     function iniciaRelogio() {
         timer = setInterval(function () {
@@ -29,13 +31,23 @@ function relogio() {
         relogio.classList.remove('timerPausado');
         titulo.innerHTML = "Timer em Execução...";
         iniciar.innerHTML = "Iniciar";
+        setTimeout(function () {
+            clearInterval(colocaRed);
+            clearInterval(removeRed);
+        })
     });
 
     pausar.addEventListener('click', function (e) {
         setTimeout(clearInterval(timer));
-        relogio.classList.add('timerPausado');
         titulo.innerHTML = "Timer Pausado";
         iniciar.innerHTML = "Retomar";
+        colocaRed = setInterval(function () {
+            relogio.classList.add('timerPausado')
+        }, 500)
+
+        removeRed = setInterval(function () {
+            relogio.classList.remove('timerPausado')
+        }, 1000)
     });
 
     zerar.addEventListener('click', function (e) {
@@ -44,6 +56,8 @@ function relogio() {
         titulo.innerHTML = "Timer";
         relogio.classList.remove('timerPausado')
         iniciar.innerHTML = "Iniciar";
+        clearInterval(colocaRed);
+        clearInterval(removeRed);
     });
 
 }
